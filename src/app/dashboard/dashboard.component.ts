f8f7652db1ff6a075fd2d9e64f3d0cecdd5ddaab
Sanket Patel <sanket.devops@gmail.com>
2022-12-18 11:13:51 -0500

    Sanket: add spinner during load chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -230,7 +230,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
 // Open new window and drow chart
   async drawChart(_id: any) {
-    this.loading = true;
     this.selectedHostMetrics = <any>undefined;
     let diskStatusChart = undefined;
     let memStatusChart = undefined;
@@ -248,14 +247,37 @@ export class DashboardComponent implements OnInit, OnDestroy {
     <html>
         <head>
             <title>${this.selectedService.hostName} / ${this.selectedService.ipAddress}</title>
+            <!-- CSS only -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+            <!-- JavaScript Bundle with Popper -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         </head>
         <body>
           <div id="autoReloadConter" style="font-size: large; font-weight: bold; text-align: center; color: red;"></div>
-          <div id="CPU" style="font-size: large; font-weight: bold;"></div>
-          <div id="uptime" style="font-size: large; font-weight: bold;"></div>
-          <div id="diskStatus" style="width:2500px; height:800px;"></div>
-          <div id="memStatus" style="width:2500px; height:800px;"></div>
-          <div id="cpuStatus" style="width:2500px; height:800px;"></div>
+          <div>
+            <div id="bSpinner" class="spinner-border" role="status">
+              <span class="sr-only"></span>
+            </div>
+            <div id="CPU" style="font-size: large; font-weight: bold;"></div>
+            <div id="uptime" style="font-size: large; font-weight: bold;"></div>
+          </div>
+          <div style="text-align: center">
+            <div id="diskStatus" style="width:2500px; height:800px;">
+              <div id="bSpinner" class="spinner-border" role="status">
+                <span class="sr-only"></span>
+              </div>
+            </div>
+            <div id="memStatus" style="width:2500px; height:800px;">
+              <div id="bSpinner" class="spinner-border" role="status">
+                <span class="sr-only"></span>
+              </div>
+            </div>
+            <div id="cpuStatus" style="width:2500px; height:800px;">
+              <div id="bSpinner" class="spinner-border" role="status">
+                <span class="sr-only"></span>
+              </div>
+            </div>
+          </div>
         </body>
     </html>`;
 
@@ -351,9 +373,10 @@ export class DashboardComponent implements OnInit, OnDestroy {
           } catch (e) {
             console.log(e);
           }
+          let bSpinner: any = chartWindow.document.getElementById("bSpinner");
+          bSpinner.style.display = 'none'
         }, 3000);
     }
     reloadChart();
-    this.loading = false;
   }
 }
